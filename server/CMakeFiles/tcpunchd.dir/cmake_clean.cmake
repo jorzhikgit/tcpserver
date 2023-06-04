e97d2b67c4455be8c6ee34e8dd1445cf14cb78a6
Sergey <sergey.kubashev@yandex.ru>
2023-06-04 12:43:35 +0600

    Add files via upload

---
+++ server/CMakeFiles/tcpunchd.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tcpunchd.dir/hole_punching_server.cpp.o"
+  "CMakeFiles/tcpunchd.dir/hole_punching_server.cpp.o.d"
+  "tcpunchd"
+  "tcpunchd.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tcpunchd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
